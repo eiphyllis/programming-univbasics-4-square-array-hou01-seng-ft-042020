0067c8626b908971aaed2b45f4faf8b48cb14683
Phyllis N <phyllisgetcity@gmail.com>
2020-04-04 19:35:04 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0 
   while counter < array.length 
   
-    new[counter] = array[counter]*array[counter]
+    new[counter] = array[counter]**2
     counter += 1 
   end
   new
